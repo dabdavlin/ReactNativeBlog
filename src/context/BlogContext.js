b0375e40a5779af730a311f47a8bfaf056cb143c
George Daclin <georgetrevord@gmail.com>
2022-07-09 20:01:04 -0500

    first commit

---
+++ src/context/BlogContext.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const BlogContext = React.createContext();
+
+export const BlogProvider = ({ children }) => {
+  const blogPosts = [{ title: "Blog Post #1" }, { title: "Blog Post #2" }];
+
+  return (
+    <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>
+  );
+};
+
+export default BlogContext;
